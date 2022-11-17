a48a42ae268093d056b2405bc65e5c6a3da9b559
Tomer <tomerbaram26@gmail.com>
2022-11-17 16:51:28 +0200

    master push

--- src/pages/ChildPage.jsx
+++ src/pages/ChildPage.jsx
@@ -1,57 +1,52 @@
+import { Text, View, StyleSheet, Pressable, Touchable } from "react-native";
 
+import axios from "axios";
+import * as React from "react";
+import { useEffect } from "react";
+import {
+  Dimensions,
+  Button,
+  ScrollView,
+  PermissionsAndroid,
+  Alert,
+} from "react-native";
+import { Platform, Linking, AppState } from "react-native";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import MapView, { Callout, Circle, Marker } from "react-native-maps";
 
-
-import {Text, View, StyleSheet, Pressable, Touchable} from 'react-native';
-
-import axios from "axios"
-import * as React from "react"
-import { useEffect } from "react"
-import { Dimensions ,Button, ScrollView, PermissionsAndroid, Alert } from "react-native"
-import {Platform,Linking,AppState} from 'react-native';
-import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete"
-import MapView, { Callout, Circle, Marker } from "react-native-maps"
-
-
-import { useState } from "react"
+import { useState } from "react";
 // import Geolocation from 'react-native-geolocation-service';
-import * as Location from 'expo-location';
-import Modal from 'react-native-modal';
-import Constants from 'expo-constants';
-import * as TaskManager from 'expo-task-manager';
-import { AsyncStorage } from 'react-native';
-import * as Notification from 'expo-notifications';
+import * as Location from "expo-location";
+import Modal from "react-native-modal";
+import Constants from "expo-constants";
+import * as TaskManager from "expo-task-manager";
+import { AsyncStorage } from "react-native";
+import * as Notification from "expo-notifications";
 // import * as Permissions from 'expo-permissions';
 import { useRef } from "react";
-import * as Application from 'expo-application';
+import * as Application from "expo-application";
 import IO from "socket.io-client";
 
+const TASK_FETCH_LOCATION = "background-location-task";
+const SERVER_URL = "http://10.195.25.104:4000";
+const USERID = "63738fb9e33a0195e497e318";
 
-const TASK_FETCH_LOCATION = 'background-location-task';
-const SERVER_URL="http://10.195.25.104:4000";
-const USERID="63738fb9e33a0195e497e318"
-
-
-
-
-const ChildPage = ({sos, setSos}) => {
-  const [sosMsg, setSosMsg] = useState(false)
-  const [sosactive, setSosactive] = useState(false)
-   const responseListener = useRef();
-
+const ChildPage = ({ sos, setSos }) => {
+  const [sosMsg, setSosMsg] = useState(false);
+  const [sosactive, setSosactive] = useState(false);
+  const responseListener = useRef();
 
   useEffect(() => {
     (async () => {
       let { status } = await Location.requestBackgroundPermissionsAsync();
-      registerForPushNotificationsAsync()
-      if (status !== 'granted') {
-        setErrorMsg('Permission to access location was denied');
-       
+      registerForPushNotificationsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+
         return;
       }
-  
     })();
   }, []);
-
 
   //   useEffect(() => {
   //   Permissions.getAsync(Permissions.NOTIFICATIONS)
@@ -66,189 +61,151 @@ const ChildPage = ({sos, setSos}) => {
   //         return;
   //       }
   //     });
-  
 
   //     // socket.on('disTo', (msg) =>{ msg<75? triggerNotification() :"",console.log(counter)});*******
   // }, []);
-  useEffect(()=> {
-
+  useEffect(() => {
     // responseListener.current = Notification.addNotificationResponseReceivedListener(response => {
     //     console.log('--- notification tapped ---');
     //     console.log(response);
     //     console.log('------');
     // });
+    // socket.emit('join')
+  }, []);
 
- 
-  // socket.emit('join')
+  useEffect(() => {
+    console.log("notification");
+    Location.startLocationUpdatesAsync(TASK_FETCH_LOCATION, {
+      accuracy: Location.Accuracy.Highest,
+      distanceInterval: 1, // minimum change (in meters) betweens updates
+      deferredUpdatesInterval: 1, // minimum interval (in milliseconds) between updates
 
-  },[])
+      // foregroundService is how you get the task to be updated as often as would be if the app was open
+      foregroundService: {
+        notificationTitle: "Using your location",
+        notificationBody:
+          "To turn off, go back to the app and switch something off.",
+      },
+    });
+  }, []);
 
+  TaskManager.defineTask(
+    TASK_FETCH_LOCATION,
+    async ({ data: { locations }, error }) => {
+      const id = "63738fb9e33a0195e497e318";
+      const token = await AsyncStorage.getItem("NotificationToken");
+      console.log(token, "**********************");
+      if (error) {
+        console.error(error);
+        return;
+      }
 
+      const [location] = locations;
+      console.log(location, "location");
 
-    
-      
-      useEffect(() => {
-        console.log("notification")
-        Location.startLocationUpdatesAsync(TASK_FETCH_LOCATION, {
-          accuracy: Location.Accuracy.Highest,
-          distanceInterval: 1, // minimum change (in meters) betweens updates
-          deferredUpdatesInterval: 1, // minimum interval (in milliseconds) between updates
-         
-          // foregroundService is how you get the task to be updated as often as would be if the app was open
-          foregroundService: {
-            notificationTitle: 'Using your location',
-            notificationBody: 'To turn off, go back to the app and switch something off.',
-          },
-        })
-      }, []);
-
-
-
-
-  
-  
-    
- 
-
-
-      TaskManager.defineTask(TASK_FETCH_LOCATION, async ({ data: { locations }, error }) => {
-        const id="63738fb9e33a0195e497e318"
-        const token= await AsyncStorage.getItem("NotificationToken")
-        console.log(token,"**********************")
-        if (error) {
-          console.error(error);
-          return;
-        }
-       
-        const [location] = locations;
-        console.log(location,"location")
-
-        try {
-    
+      try {
         //   socket.emit('disOn', location,USERID)// you should use post instead of get to persist data on the backend
         // axios.patch(SERVER_URL+"/api-map/users/parent/pushNotification",{id:id,token:token,location:location})
-        axios.patch(SERVER_URL+"/api-map/users/parent/addChildrenLocation",{id:id,connectionToken:"c8b682c1-cb6b",
-        currentLocation:location,token:"ExponentPushToken[Uh8EfSGwGP2wOYky3ImWmQ]"})
-          console.log(location.coords.latitude)
-         
-        } catch (err) {
-          console.error(err);
-        }
-      });
+        axios.patch(SERVER_URL + "/api-map/users/parent/addChildrenLocation", {
+          id: id,
+          connectionToken: "c8b682c1-cb6b",
+          currentLocation: location,
+          token: "ExponentPushToken[Uh8EfSGwGP2wOYky3ImWmQ]",
+        });
+        console.log(location.coords.latitude);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+  );
 
-
-
-
-
-
-  const longhandle =  () => {
-    alert('SOS called!')
-    setSosMsg(false)
-    setSos(true)
-  }
+  const longhandle = () => {
+    alert("SOS called!");
+    setSosMsg(false);
+    setSos(true);
+  };
 
   const pressIn = () => {
-    setSosMsg(true)
-  }
+    setSosMsg(true);
+  };
 
   const pressOut = () => {
-    setSosMsg(false)
-  }
-  
-  return(
-    <View style ={ styles.container}>
-  <Pressable
-  style={styles.trackBtn}
-  >
-    <Text
-    style={styles.trackText}
-    >
-      START
-    </Text>
-    </Pressable>
+    setSosMsg(false);
+  };
 
-    <Pressable
-  style={sosMsg ? styles.sosBtnActive :  styles.sosBtn}
-  delayLongPress={3000}
-  onLongPress={longhandle}
-  onPressIn={pressIn}
-  onPressOut={pressOut}
-  >
-    <Text
-    style={styles.sosText}>
-      S O S
-    </Text>
-    </Pressable>
-    {sosMsg &&(
-      <Text
-      style={styles.sosMsg}
-      >
-        Hold for 3 seconds
-      </Text>
-    )}
-  </View>
+  return (
+    <View style={styles.container}>
+      <Pressable style={styles.trackBtn}>
+        <Text style={styles.trackText}>START</Text>
+      </Pressable>
 
-
-)};
-
-
+      <Pressable
+        style={sosMsg ? styles.sosBtnActive : styles.sosBtn}
+        delayLongPress={3000}
+        onLongPress={longhandle}
+        onPressIn={pressIn}
+        onPressOut={pressOut}>
+        <Text style={styles.sosText}>S O S</Text>
+      </Pressable>
+      {sosMsg && <Text style={styles.sosMsg}>Hold for 3 seconds</Text>}
+    </View>
+  );
+};
 
 export default ChildPage;
 
 const styles = StyleSheet.create({
-  container:{
-    textAlign:'center'
+  container: {
+    textAlign: "center",
   },
-  trackBtn:{
-    height:200,
-    width:200,
-    backgroundColor:"#577399",
-    borderRadius:100,
-    display:'flex',
-    justifyContent:'center',
-    alignItems:'center',
-    alignContent:'center',
-    marginTop:200,
+  trackBtn: {
+    height: 200,
+    width: 200,
+    backgroundColor: "#577399",
+    borderRadius: 100,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    alignContent: "center",
+    marginTop: 200,
   },
-  trackText:{
-    color:'white',
-    fontWeight:"800",
-    fontSize:35,
-    textAlign:'center'
+  trackText: {
+    color: "white",
+    fontWeight: "800",
+    fontSize: 35,
+    textAlign: "center",
   },
-  sosBtn:{
-    backgroundColor:"#D51807",
-    display:'flex',
-    justifyContent:'center',
-    alignItems:'center',
-    alignContent:'center',
-    padding:15,
-    borderRadius:10,
-    marginLeft:0,
-    marginTop:90,
+  sosBtn: {
+    backgroundColor: "#D51807",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    alignContent: "center",
+    padding: 15,
+    borderRadius: 10,
+    marginLeft: 0,
+    marginTop: 90,
   },
-  
-  sosText:{
-    color:'white',
-    fontWeight:"800",
+
+  sosText: {
+    color: "white",
+    fontWeight: "800",
   },
-  sosMsg : {
+  sosMsg: {
     marginTop: 100,
-    textAlign:'center'
+    textAlign: "center",
   },
-  sosBtnActive:{
-    backgroundColor:"#D51807",
-    display:'flex',
-    justifyContent:'center',
-    alignItems:'center',
-    alignContent:'center',
-    borderRadius:10,
-    marginLeft:0,
-    marginTop:90,
-    padding:20,
-    borderColor:'#c91100',
-    borderWidth:4
-  }
+  sosBtnActive: {
+    backgroundColor: "#D51807",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    alignContent: "center",
+    borderRadius: 10,
+    marginLeft: 0,
+    marginTop: 90,
+    padding: 20,
+    borderColor: "#c91100",
+    borderWidth: 4,
+  },
 });
-
-
--- src/pages/MainMap.jsx
+++ src/pages/MainMap.jsx
@@ -1,332 +1,317 @@
-
-import axios from "axios"
-import * as React from "react"
-import { useEffect } from "react"
-import { Dimensions, StyleSheet, Text, View,Button, ScrollView, PermissionsAndroid, Alert, Platform } from "react-native"
-import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete"
-import MapView, { Callout, Circle, Marker } from "react-native-maps"
+import axios from "axios";
+import * as React from "react";
+import { useEffect } from "react";
+import {
+  Dimensions,
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  ScrollView,
+  PermissionsAndroid,
+  Alert,
+  Platform,
+} from "react-native";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import MapView, { Callout, Circle, Marker } from "react-native-maps";
 // import Geolocation from 'react-native-geolocation-service'r
 import { useState } from "react";
-import * as Location from 'expo-location';
-import * as TaskManager from 'expo-task-manager';
-import { AsyncStorage } from 'react-native';
-import * as Notification from 'expo-notifications';
-import * as Permissions from 'expo-permissions';
+import * as Location from "expo-location";
+import * as TaskManager from "expo-task-manager";
+import { AsyncStorage } from "react-native";
+import * as Notification from "expo-notifications";
+import * as Permissions from "expo-permissions";
 import { useRef } from "react";
-import { TextInput } from "react-native-paper"
+import { TextInput } from "react-native-paper";
 import IO, { Socket } from "socket.io-client";
 
+const TASK_FETCH_LOCATION = "background-location-task";
+const SERVER_URL = "http://10.195.25.104:4000";
 
-const TASK_FETCH_LOCATION = 'background-location-task';
-const SERVER_URL="http://10.195.25.104:4000";
-
-const USERID="63738fb9e33a0195e497e318"
-
+const USERID = "63738fb9e33a0195e497e318";
 
 Notification.setNotificationHandler({
-	handleNotification: async () => {
-	  return {
-		shouldShowAlert: true,
-		shouldPlaySound: true
-	  };
-	}
-  });
-  
-
-  const socket = IO(SERVER_URL, {
+  handleNotification: async () => {
+    return {
+      shouldShowAlert: true,
+      shouldPlaySound: true,
+    };
+  },
 });
 
+const socket = IO(SERVER_URL, {});
 
 async function registerForPushNotificationsAsync() {
-	let token;
-  
-	const { status: existingStatus } = await Notification.getPermissionsAsync();
-	let finalStatus = existingStatus;
-  
-	if (existingStatus !== 'granted') {
-		const { status } = await Notification.requestPermissionsAsync();
-		finalStatus = status;
-	}
-	if (finalStatus !== 'granted') {
-		alert('Failed to get push token for push notification!');
-		return;
-	}
-	if (finalStatus == 'granted') {
-		if(Platform.OS==="android")
-		{
-			Notification.setNotificationChannelAsync("default",{
-				name:"default",
-				importance:Notification.AndroidImportance.MAX
-			})
-		}
-	}
-	token = (await Notification.getExpoPushTokenAsync()).data;
-	console.log(token+"%%%%%%%%%%%%%%%%%%%%%%%%");
-  
-	await AsyncStorage.setItem("NotificationToken",`${token}`)
-  
-  
-	alert(await AsyncStorage.getItem("NotificationToken"))
+  let token;
+
+  const { status: existingStatus } = await Notification.getPermissionsAsync();
+  let finalStatus = existingStatus;
+
+  if (existingStatus !== "granted") {
+    const { status } = await Notification.requestPermissionsAsync();
+    finalStatus = status;
   }
+  if (finalStatus !== "granted") {
+    alert("Failed to get push token for push notification!");
+    return;
+  }
+  if (finalStatus == "granted") {
+    if (Platform.OS === "android") {
+      Notification.setNotificationChannelAsync("default", {
+        name: "default",
+        importance: Notification.AndroidImportance.MAX,
+      });
+    }
+  }
+  token = (await Notification.getExpoPushTokenAsync()).data;
+  console.log(token + "%%%%%%%%%%%%%%%%%%%%%%%%");
 
+  await AsyncStorage.setItem("NotificationToken", `${token}`);
 
-
-
-
-
-
+  alert(await AsyncStorage.getItem("NotificationToken"));
+}
 
 export default function MainMap() {
-     
   const [location, setLocation] = useState(null);
   const [errorMsg, setErrorMsg] = useState(null);
   const [latitude, setLatitude] = useState(null);
   const [longitude, setLongitude] = useState(null);
-  const[kidsLocations,setKidsLocations]=useState([])//To do add socket.on that changes kids array and show on map
+  const [kidsLocations, setKidsLocations] = useState([]); //To do add socket.on that changes kids array and show on map
 
+  const [myLocatin, setMyLocation] = useState({
+    latitude: 32.07962,
+    longitude: 34.88911,
+  });
 
-  const[myLocatin,setMyLocation]=useState({
-		latitude: 32.07962,
-		longitude: 34.88911
-	})
+  const [pin, setPin] = React.useState({
+    latitude: 32.07962,
+    longitude: 34.88911,
+  });
 
-
-
-	const [ pin, setPin ] = React.useState({
-		latitude: 32.07962,
-		longitude: 34.88911
-	})
-  
-	const [ region, setRegion ] = React.useState({
-		latitude: 32.07962,
-		longitude: 34.88911,
-		latitudeDelta: 0.0922,
-		longitudeDelta: 0.0421
-	})
-  const [ baseLocations, setBaseLocations ] = React.useState([])
+  const [region, setRegion] = React.useState({
+    latitude: 32.07962,
+    longitude: 34.88911,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  });
+  const [baseLocations, setBaseLocations] = React.useState([]);
   const responseListener = useRef();
 
-  useEffect(()=>{
-	const id="63738fb9e33a0195e497e318"
+  useEffect(() => {
+    const id = "63738fb9e33a0195e497e318";
 
-	socket.on('connection', function(socket){
-		console.log(`${socket.id} is connected`)
-		
-		socket.on(`${id}`, (socketKidsLocations) => {
-		  socket.join(socket.id)
-		  console.log("socket**************************")
-		 setKidsLocations([...socketKidsLocations])
-		})
-	  
-	  
-	//   console.log("***")
-	// 	socket.on('disOn', (location,id) => {
-	// 	  console.log("on")
-	// 	  socket.emit('disTo', getDis(location,String(id)))
-	// 	  console.log(`user ${socket.id} joined room ${socket.id}`);
-	// 	})
-	   
-	  
-	  socket.on('disconnect',()=>{
-		console.log("user"+socket.id+" disconnected")
-	  })
+    socket.on("connection", function (socket) {
+      console.log(`${socket.id} is connected`);
 
-	  });
- })
- useEffect(()=> {
+      socket.on(`${id}`, (socketKidsLocations) => {
+        socket.join(socket.id);
+        console.log("socket**************************");
+        setKidsLocations([...socketKidsLocations]);
+      });
 
-    responseListener.current = Notification.addNotificationResponseReceivedListener(response => {
-        console.log('--- notification tapped ---');
+      //   console.log("***")
+      // 	socket.on('disOn', (location,id) => {
+      // 	  console.log("on")
+      // 	  socket.emit('disTo', getDis(location,String(id)))
+      // 	  console.log(`user ${socket.id} joined room ${socket.id}`);
+      // 	})
+
+      socket.on("disconnect", () => {
+        console.log("user" + socket.id + " disconnected");
+      });
+    });
+  });
+  useEffect(() => {
+    responseListener.current =
+      Notification.addNotificationResponseReceivedListener((response) => {
+        console.log("--- notification tapped ---");
         console.log(response);
-        console.log('------');
-    })
-},[])
-
-
- useEffect(()=>{
-	const id="63738fb9e33a0195e497e318"
-		 axios.post(SERVER_URL+"/api-map/users/parent/getChildrenLocation",{id:id})
-	.then(data=>setKidsLocations(data.data))
- },[])
-
- 
-
+        console.log("------");
+      });
+  }, []);
 
   useEffect(() => {
-    const id="63738fb9e33a0195e497e318"
-      axios.post(SERVER_URL+"/api-map/users/parent/getBaseLocations",{id:id})
-       
-       .then((data) => 
-       
-        { setBaseLocations(data.data)
-         
-        }
-       ).catch(error => console.log(error));
- }, []); 
+    const id = "63738fb9e33a0195e497e318";
+    axios
+      .post(SERVER_URL + "/api-map/users/parent/getChildrenLocation", {
+        id: id,
+      })
+      .then((data) => setKidsLocations(data.data));
+  }, []);
 
+  useEffect(() => {
+    const id = "63738fb9e33a0195e497e318";
+    axios
+      .post(SERVER_URL + "/api-map/users/parent/getBaseLocations", { id: id })
 
+      .then((data) => {
+        setBaseLocations(data.data);
+      })
+      .catch((error) => console.log(error));
+  }, []);
 
-
-
-	return (
-		<View 	style={styles.container}>
-  
-			<GooglePlacesAutocomplete
-				placeholder="Search"
-				fetchDetails={true}
-				GooglePlacesSearchQuery={{
-					rankby: "distance"
-				}}
-				onPress={(data, details = null) => {
-					// 'details' is provided when fetchDetails = true
-					console.log(data, details)
-					setRegion({
-						latitude: details.geometry.location.lat,
-						longitude: details.geometry.location.lng,
-						latitudeDelta: 0.0922,
-						longitudeDelta: 0.0421
-					})
-				}}
-				query={{
-					// key: "AIzaSyDJheGFKSCMaY62ohf_eld0gq171hcY0F4",
+  return (
+    <View style={styles.container}>
+      <GooglePlacesAutocomplete
+        placeholder="Search"
+        fetchDetails={true}
+        GooglePlacesSearchQuery={{
+          rankby: "distance",
+        }}
+        onPress={(data, details = null) => {
+          // 'details' is provided when fetchDetails = true
+          console.log(data, details);
+          setRegion({
+            latitude: details.geometry.location.lat,
+            longitude: details.geometry.location.lng,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          });
+        }}
+        query={{
+          // key: "AIzaSyDJheGFKSCMaY62ohf_eld0gq171hcY0F4",
           key: "none",
-					language: "iw",
-					components: "country:il",
-					types: "address",
-					radius: 100,
-					location: `${region.latitude}, ${region.longitude}`
-				}}
-				// styles={{
-				// 	container: { flex: 0, position: "relative", width: "100%", zIndex: 1 },
-				// 	listView: { backgroundColor: "white" }
-				// }}
-       
+          language: "iw",
+          components: "country:il",
+          types: "address",
+          radius: 100,
+          location: `${region.latitude}, ${region.longitude}`,
+        }}
+        // styles={{
+        // 	container: { flex: 0, position: "relative", width: "100%", zIndex: 1 },
+        // 	listView: { backgroundColor: "white" }
+        // }}
+
         minLength={2}
         autoFocus={false}
-        returnKeyType={'default'}
-       
+        returnKeyType={"default"}
         styles={{
-           	container: { flex: 0, position: "relative", width: "100%", zIndex: 1},
+          container: {
+            flex: 0,
+            position: "relative",
+            width: "100%",
+            zIndex: 1,
+          },
           textInputContainer: {
-            backgroundColor: '#d3d3d3',
+            backgroundColor: "#d3d3d3",
           },
           textInput: {
             height: 38,
-            color: '#5d5d5d',
+            color: "#5d5d5d",
             fontSize: 16,
           },
           predefinedPlacesDescription: {
-            color: '#1faadb',
-          }
+            color: "#1faadb",
+          },
         }}
-			/>
-       
-			<MapView
-				style={styles.map}
-				initialRegion={{
+      />
+
+      <MapView
+        style={styles.map}
+        initialRegion={{
           latitude: 32.07962,
           longitude: 34.88911,
-					latitudeDelta: 0.0922,
-					longitudeDelta: 0.0421
-				}}
-				provider="google"
-			>
-	
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+        provider="google">
+        <Marker
+          coordinate={pin}
+          pinColor="black"
+          draggable={true}
+          onDragStart={(e) => {
+            console.log("Drag start", e.nativeEvent.coordinate);
+          }}
+          onDragEnd={(e) => {
+            setPin({
+              latitude: e.nativeEvent.coordinate.latitude,
+              longitude: e.nativeEvent.coordinate.longitude,
+            });
+          }}>
+          <Callout>
+            <Text>I'm here</Text>
+          </Callout>
+        </Marker>
+        <Circle center={pin} radius={35} />
 
-
-
-				<Marker
-					coordinate={pin}
-					pinColor="black"
-					draggable={true}
-					onDragStart={(e) => {
-						console.log("Drag start", e.nativeEvent.coordinate)
-					}}
-					onDragEnd={(e) => {
-						setPin({
-							latitude: e.nativeEvent.coordinate.latitude,
-							longitude: e.nativeEvent.coordinate.longitude
-						})
-					}}
-				>
-
-
-
-					<Callout>
-						<Text>I'm here</Text>
-					</Callout>
-				</Marker>
-				<Circle center={pin} radius={35} />
-
-       
-
-
-				{kidsLocations.map((marker, index) => (
-					
-				marker.location[marker.location.length-1]?	
-          <>
-    <Marker
-      key={index}
-      coordinate={{ latitude: parseFloat(marker.location[marker.location.length-1].latitude)
-		, longitude: parseFloat(marker.location[marker.location.length-1].longitude) }}
-        title={marker.connectionToken}
-    />
-    <Circle key ={index+199} center={{ latitude: parseFloat(marker.location[marker.location.length-1].latitude)
-		, longitude: parseFloat(marker.location[marker.location.length-1].latitude) }}
-         radius={75} />
-		
-    </>
-	:""
-  ))}
-
-
-
+        {kidsLocations.map((marker, index) =>
+          marker.location[marker.location.length - 1] ? (
+            <>
+              <Marker
+                key={index}
+                coordinate={{
+                  latitude: parseFloat(
+                    marker.location[marker.location.length - 1].latitude
+                  ),
+                  longitude: parseFloat(
+                    marker.location[marker.location.length - 1].longitude
+                  ),
+                }}
+                title={marker.connectionToken}
+              />
+              <Circle
+                key={index + 199}
+                center={{
+                  latitude: parseFloat(
+                    marker.location[marker.location.length - 1].latitude
+                  ),
+                  longitude: parseFloat(
+                    marker.location[marker.location.length - 1].latitude
+                  ),
+                }}
+                radius={75}
+              />
+            </>
+          ) : (
+            ""
+          )
+        )}
 
         {baseLocations.map((marker, index) => (
           <>
-    <Marker
-      key={index}
-      coordinate={{ latitude: parseFloat(marker.latitude), longitude: parseFloat(marker.longitude) }}
-        title={marker.name&&marker.name}
-    />
-    <Circle key ={index+199} center={{ latitude: parseFloat(marker.latitude), longitude: parseFloat(marker.longitude) }} radius={75} />
-
-   
-
-
-    </>
-  ))}
-        
-			</MapView>
-      <View style={{ marginTop: 50, flex: 1 ,flexDirection:"column"}}>
-      
-      <Text> {"mainMap"}</Text>
-   
-      
+            <Marker
+              key={index}
+              coordinate={{
+                latitude: parseFloat(marker.latitude),
+                longitude: parseFloat(marker.longitude),
+              }}
+              title={marker.name && marker.name}
+            />
+            <Circle
+              key={index + 199}
+              center={{
+                latitude: parseFloat(marker.latitude),
+                longitude: parseFloat(marker.longitude),
+              }}
+              radius={75}
+            />
+          </>
+        ))}
+      </MapView>
+      <View style={{ marginTop: 50, flex: 1, flexDirection: "column" }}>
+        <Text> {"mainMap"}</Text>
       </View>
-     
-		</View>
-	)
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: "#fff",
-		alignItems: "center",
-		justifyContent: "center"
-	},
-	map: {
-		width: Dimensions.get("window").width,
-		height: Dimensions.get("window").height/2
-	},
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  map: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height / 2,
+  },
   inputStyle: {
-    borderColor: '#16213E',
+    borderColor: "#16213E",
     borderWidth: 1,
     borderRadius: 10,
     padding: 10,
-    height:50,
-    width:200
-
-  }
-})
+    height: 50,
+    width: 200,
+  },
+});
