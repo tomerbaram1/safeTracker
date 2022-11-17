1a13b47d8a9d09fb9c9d1c2cf7e7235b7abd414c
עידו תשובה <idoteshuva@h-MacBook-Pro-sl-ydw.local>
2022-11-17 16:44:48 +0200

    sos fix and ui library

--- src/pages/EntryScreen.jsx
+++ src/pages/EntryScreen.jsx
@@ -2,60 +2,61 @@
 // import { useState } from 'react';
 
 //navigation
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 const Stack = createNativeStackNavigator();
 
 //components
 
-import WelcomePage from './WelcomePage';
-import SignIn from './SignIn';
-import SignUp from './SignUp';
-import Content from './Content';
-import { useSelector } from 'react-redux';
-import { useEffect } from 'react';
+import WelcomePage from "./WelcomePage";
+import SignIn from "./SignIn";
+import SignUp from "./SignUp";
+import Content from "./Content";
+import { useSelector } from "react-redux";
+import { useEffect } from "react";
 
 const EntryScreen = () => {
-    const { user } = useSelector((state) => state.auth);
+  const { user } = useSelector((state) => state.auth);
 
-    // const [User, setUser] = useState(false)
-                            
-    return (
-        // <Stack.Navigator>
-        //     { (User) ?
-        //         (    
-        //             <Stack.Screen name="Content" component={Content} options={{headerShown:false}}/>  
-        //         ) :
-        //         (
-        //             <Stack.Screen name="WelcomePage" component={WelcomePage} options={{headerShown:false}}/>
-        //             <Stack.Screen name="SignUp" component={SignUp} options={{headerShown:false}}/>
-        //             <Stack.Screen name="SignIn" component={SignIn} options={{headerShown:false}}/>  
-        //         )
-        //     }
-        // </Stack.Navigator>
-        <>
-        
+  // const [User, setUser] = useState(false)
 
-    
- <Stack.Navigator> 
-    
-     <Stack.Screen name="WelcomePage" component={WelcomePage} options={{headerShown:false}}/>
-     <Stack.Screen name="SignUp" component={SignUp} options={{headerShown:false}}/>
-     <Stack.Screen name="SignIn" component={SignIn} options={{headerShown:false}}/>  
- 
-
-
-    
-<Stack.Screen name="Content" component={Content} options={{headerShown:false}}/>  
-    </Stack.Navigator>
-
-           
-            
-       
-        
-        
-        </>
-        
-        )
-    }
+  return (
+    // <Stack.Navigator>
+    //     { (User) ?
+    //         (
+    //             <Stack.Screen name="Content" component={Content} options={{headerShown:false}}/>
+    //         ) :
+    //         (
+    //             <Stack.Screen name="WelcomePage" component={WelcomePage} options={{headerShown:false}}/>
+    //             <Stack.Screen name="SignUp" component={SignUp} options={{headerShown:false}}/>
+    //             <Stack.Screen name="SignIn" component={SignIn} options={{headerShown:false}}/>
+    //         )
+    //     }
+    // </Stack.Navigator>
+    <>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="WelcomePage"
+          component={WelcomePage}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="SignUp"
+          component={SignUp}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="SignIn"
+          component={SignIn}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Content"
+          component={Content}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </>
+  );
+};
 
 export default EntryScreen;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit"
 import AuthReducer from "./AuthSlice"
+import SosReducer from "./SosSlice"
 export const store = configureStore({
     reducer:{
       auth:AuthReducer,
-      
+      sos:SosReducer
 
     },
 })
