00847ae5965b48cb50183257d9133e2bb58ac607
Tomer <tomerbaram26@gmail.com>
2022-11-23 09:42:26 +0200

    settings merged

--- src/pages/AddLocation.jsx
+++ src/pages/AddLocation.jsx
@@ -21,7 +21,6 @@ const TASK_FETCH_LOCATION = 'background-location-task';
 
 
 const SERVER_URL="http://172.20.10.4:4000";
-const SERVER_URL="http://172.20.10.3:4000";
 
 
 const USERID="63738fb9e33a0195e497e318"
